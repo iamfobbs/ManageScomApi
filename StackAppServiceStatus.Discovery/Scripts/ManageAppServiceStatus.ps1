b1c1aad76dc23d5cf038587a4f69913cfe5dfe6c
Dennis Ofoborh <dennis@iamfobbs.co.uk>
2016-01-07 18:01:26 +0000

    instance variables for discovery script

--- StackAppServiceStatus.Discovery/Scripts/ManageAppServiceStatus.ps1
+++ StackAppServiceStatus.Discovery/Scripts/ManageAppServiceStatus.ps1
@@ -5,7 +5,7 @@ param($sourceId,$managedEntityId)
 
 ## Authentication variables
 $user = "darthmaul"
-$pwd = "!deathStar" | ConvertTo-SecureString -asPlainText -Force
+$pwd = "!deathstar" | ConvertTo-SecureString -asPlainText -Force
 $limit = 1
 
 $api = New-Object -comObject 'MOM.ScriptAPI'
@@ -13,7 +13,7 @@ $discoveryData = $api.CreateDiscoveryData(0, $sourceId, $managedEntityId)
 #Log script event
 #$api.LogScriptEvent('Scripts/ManageAppServiceStatus.ps1',999,4,$service)
 
-$defUrl = "https://demo1.ravn.co.uk/ravn-manage/api/v2/service_states?limit=$limit&offset=$offset"
+$defUrl = "https://demo1.galacticempire.co.uk/tie-fighter/api/v2/service_states?limit=$limit&offset=$offset"
 $cred = New-Object System.Management.Automation.PSCredential($user,$pwd)
 $stackStatus = Invoke-RestMethod -Uri $defUrl -Credential $cred
 
@@ -28,10 +28,8 @@ if ($stackStatus.Count -ge 1)
     #for each service found perform an addition call to determine the status of the service. 
     foreach ($Url in $UrlsParsed)
     {
+		#create discoverty instance
         $instance = $discoveryData.CreateClassInstance("$MPElement[Name='StackAppServiceStatus.Discovery.ProbeService']$")
-
-
-
 
         $sep = Invoke-WebRequest -uri $url -Credential $cred
         #collects first different data sets
@@ -47,9 +45,15 @@ if ($stackStatus.Count -ge 1)
         $svcStatus | ft
         $offset++
 
+		$instance.AddProperty("$MPElement[Name='StackAppServiceStatus.Discovery.ProbeService']/PrincipalName$", $computerName)
 
-		
-        $instance.AddProperty("$MPElement[StackAppServiceStatus.DiscoveryService]")
+		$instance.AddProperty("$MPElement[Name='StackAppServiceStatus.Discovery.ProbeService']/Response$", 'Response' + $svcStatus.response)
+		$instance.AddProperty("$MPElement[Name='StackAppServiceStatus.Discovery.ProbeService']/Status$", 'Status' + $svcStatus.status)
+		$instance.AddProperty("$MPElement[Name='StackAppServiceStatus.Discovery.ProbeService']/Displayname$", 'Displayname' + $svcStatus.displayname)
+		$instance.AddProperty("$MPElement[Name='StackAppServiceStatus.Discovery.ProbeService']/Port$", 'Port' + $svcStatus.port)
+
+		#store all variables into instance
+		$discoveryData.AddInstance($instance)
             
     }
 
@@ -60,23 +64,63 @@ if ($stackStatus.Count -ge 1)
    
 } 
 
+$discoveryData
 
 
+### UNMODIFIED SCRIPTS [parameters for script discovery not yet added] - Works! ####
+######### MULTIPLE SERVICES #############
+#$user = "darthmaul"
+#$pwd = "!deathstar" | ConvertTo-SecureString -asPlainText -Force
+#$limit = 1
 
+#function GetServiceStates($offset)
+#{
+   
+#    $defUrl = "https://demo1.galacticempire.co.uk/tie-fighter/api/v2/service_states?limit=$limit&offset=$offset"
+#    $cred = New-Object System.Management.Automation.PSCredential($user,$pwd)
+#    #$stackStatus = Invoke-WebRequest -Uri $url -Credential $cred
+#    $stackStatus = Invoke-RestMethod -Uri $defUrl -Credential $cred
 
+#    if ($stackStatus.Count -ge 1) 
+#	{
+#        #appends only the service url for an individual service
+#        $parsedUrls = $stackStatus.serviceUrl
+#        #secure requests required else the call fails
+#        $UrlsParsed = $parsedUrls.Replace("http","https")
+#        #$UrlsParsed 
 
+#        #for each service found perform an addition call to determine the status of the service. 
+#        foreach ($Url in $UrlsParsed)
+#        {
+#            $sep = Invoke-WebRequest -uri $url -Credential $cred
+#            $svcStatus = $sep | select @{name="Response";Expression ={$_.statuscode}}, @{name="Status";Expression ={$_.statusdescription}} 
+#            $svcCall = $sep | ConvertFrom-Json | select displayname, serviceTypeUrl, port, serviceport
 
+#            $svcStatus | Add-Member -MemberType NoteProperty -Name displayname -Value $svcCall.displayname
+#            $svcStatus | Add-Member -MemberType NoteProperty -Name serviceTypeUrl -Value $svcCall.serviceTypeUrl
+#            $svcStatus | Add-Member -MemberType NoteProperty -Name port -Value $svcCall.port
+#            $svcStatus | Add-Member -MemberType NoteProperty -Name servicePort -Value $svcCall.servicePort
+#            $svcStatus | ft
+			
+#			#increment offset
+#            $offset++
 
+#        }
 
+#            if($offset -ge $limit)
+#			{
+#                GetServiceStates($offset)
+#            }
+   
+#    } 
+#}
 
-
-
-
+#GetServiceStates(0) 
 
 ########## MULTIPLE SERVICES #############
-#$user = "deathMaul"
+#$user = "denniso"
 #$pwd = "*******" | ConvertTo-SecureString -asPlainText -Force
-#$defUrl = "https://demo1.ravn.co.uk/ravn-manage/api/v2/service_states?limit=1000"
+#$defUrl = "https://demo1.galacticempire.co.uk/tie-fighter/api/v2/service_states?limit=1000"
 #$cred = New-Object System.Management.Automation.PSCredential($user,$pwd)
 ##$stackStatus = Invoke-WebRequest -Uri $url -Credential $cred
 #$stackStatus = Invoke-RestMethod -Uri $defUrl -Credential $cred
@@ -101,6 +145,3 @@ if ($stackStatus.Count -ge 1)
     
 #    $svcStatus | ft
 #}
-
-
-
